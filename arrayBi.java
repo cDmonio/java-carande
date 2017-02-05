a2a16eea43d6f83927e6bb24109877a05ade9f68
prueba <example@example.com>
2017-02-06 00:04:05 +0100

    acabar arrayBi.java

--- arrayBi.java
+++ arrayBi.java
@@ -4,27 +4,48 @@ import java.util.Arrays;
 class arrayBi {
 
     public static void main(String[] args) {
-        crearArray();
+        double[][] array = rellenarArray(crearArray());
 
 
 
     }
 
-    public static int[][] crearArray() {
-      Scanner scanner = new Scanner(System.in);
+    public static double[][] crearArray() {
+        Scanner scanner = new Scanner(System.in);
 
-      try {
-          System.out.print("Introduce el numero alumnos: ");
-          int alumnos = scanner.nextInt();
-          System.out.print("Introduce el numero de notas por alumno: ");
-          int notas = scanner.nextInt();
+        try {
+            System.out.print("Introduce el numero alumnos: ");
+            int alumnos = scanner.nextInt();
+            System.out.print("Introduce el numero de notas por alumno: ");
+            int notas = scanner.nextInt();
 
-          int[][] arrayClase = new int[alumnos][notas+1];
-          System.out.println(arrayClase.length+" "+arrayClase[0].length);
-          System.out.println(Arrays.deepToString(arrayClase));
-          return arrayClase;
+            double[][] arrayClase = new double[alumnos][notas+1];
+            // System.out.println(arrayClase.length+" "+arrayClase[0].length);
+            // System.out.println(Arrays.deepToString(arrayClase));
+            return arrayClase;
 
-      } catch (java.util.InputMismatchException e) { return crearArray(); }
+        } catch (java.util.InputMismatchException e) { return crearArray(); }
 
     }
+
+    public static double[][] rellenarArray(double[][] array) {
+        double total = 0;
+
+        for (int a = 0 ; a < array.length ; a++) {
+            // System.out.println(a);
+            for (int n = 0 ; n < array[a].length-1 ; n++) {
+                // System.out.println("\t"+n);
+                array[a][n] = (int) (Math.random() * 1001) / 100.0;
+                total += array[a][n];
+            }
+            //calcular la media y asignarlo a la ultima posicion del array anidado
+            array[a][array[a].length-1] = Math.round(total/(array[a].length-1)*100)/100.0;
+            total = 0;
+        }
+        System.out.println(Arrays.deepToString(array));
+        return array;
+    }
+
+
+
 }
